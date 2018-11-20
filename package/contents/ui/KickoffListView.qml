LemonByte32/kickoff-with-submenus

commit ab83a41d2ad228aad636542c30c99d2c88dadccb

Port Kickoff to shared view component using single-MouseArea pattern
Summary:
This:
* Moves MouseArea out of the item delegate and uses a single MouseArea
  per view.
* Ports all the pages to using a shared view component.

Aside from saving a lot of QObjects, using a single MouseArea also
has the advantage that MouseArea.positionChanged isn't fired when
QQuickWindow synthesizes a hover event per frame, unlike MouseArea
.containsMouse. This fixes a user-reported bug in the search page,
and the same pattern is used by various other UIs we have for
similar reasons.

BUG:397693

Reviewers: #plasma, ngraham, davidedmundson

Reviewed By: #plasma, davidedmundson

Subscribers: davidedmundson, ngraham, plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D15855

package/contents/ui/KickoffListView.qml
@@ -1,0 +1,170 @@
+/*
+    Copyright (C) 2011  Martin Gräßlin <mgraesslin@kde.org>
+    Copyright (C) 2012  Gregor Taetzner <gregor@freenet.de>
+    Copyright (C) 2015-2018  Eike Hein <hein@kde.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+import QtQuick 2.0
+import org.kde.plasma.core 2.0 as PlasmaCore
+import org.kde.plasma.extras 2.0 as PlasmaExtras
+import org.kde.plasma.components 2.0 as PlasmaComponents
+
+
+FocusScope {
+    id: view
+
+    signal reset
+    signal addBreadcrumb(var model, string title)
+
+    readonly property Item listView: listView
+    readonly property Item scrollArea: scrollArea
+
+    property bool showAppsByName: true
+    property bool appView: false
+
+    property alias model: listView.model
+    property alias delegate: listView.delegate
+    property alias currentIndex: listView.currentIndex
+    property alias currentItem: listView.currentItem
+    property alias count: listView.count
+    property alias interactive: listView.interactive
+    property alias contentHeight: listView.contentHeight
+
+    property alias move: listView.move
+    property alias moveDisplaced: listView.moveDisplaced
+
+    function incrementCurrentIndex() {
+        listView.incrementCurrentIndex();
+    }
+
+    function decrementCurrentIndex() {
+        listView.decrementCurrentIndex();
+    }
+
+    PlasmaExtras.ScrollArea {
+        id: scrollArea
+
+        anchors.fill: parent
+
+        ListView {
+            id: listView
+
+            focus: true
+
+            keyNavigationWraps: true
+            boundsBehavior: Flickable.StopAtBounds
+
+            highlight: KickoffHighlight {}
+            highlightMoveDuration : 0
+            highlightResizeDuration: 0
+
+            delegate: KickoffItem {
+                id: delegateItem
+
+                appView: view.appView
+                showAppsByName: view.showAppsByName
+
+                onReset: view.reset()
+                onAddBreadcrumb: view.addBreadcrumb(model, title)
+            }
+
+            section {
+                property: "group"
+                criteria: ViewSection.FullString
+                delegate: SectionDelegate {}
+            }
+        }
+    }
+
+    MouseArea {
+        anchors.fill: parent
+
+        id: mouseArea
+
+        property Item pressed: null
+        property int pressX: -1
+        property int pressY: -1
+
+        hoverEnabled: true
+        acceptedButtons: Qt.LeftButton | Qt.RightButton
+
+        onPressed: {
+            var mapped = listView.mapToItem(listView.contentItem, mouse.x, mouse.y);
+            var item = listView.itemAt(mapped.x, mapped.y);
+
+            if (!item) {
+                return;
+            }
+
+            if (mouse.buttons & Qt.RightButton) {
+                if (item.hasActionList) {
+                    mapped = listView.contentItem.mapToItem(item, mapped.x, mapped.y);
+                    openActionMenu(mapped.x, mapped.y);
+                }
+            } else {
+                pressed = item;
+                pressX = mouse.x;
+                pressY = mouse.y;
+            }
+        }
+
+        onReleased: {
+            var mapped = listView.mapToItem(listView.contentItem, mouse.x, mouse.y);
+            var item = listView.itemAt(mapped.x, mapped.y);
+
+            if (item && pressed === item) {
+                if (item.appView) {
+                    view.state = "OutgoingLeft";
+                } else {
+                    item.activate();
+                }
+
+                listView.currentIndex = -1;
+            }
+
+            pressed = null;
+            pressX = -1;
+            pressY = -1;
+        }
+
+        onPositionChanged: {
+            var mapped = listView.mapToItem(listView.contentItem, mouse.x, mouse.y);
+            var item = listView.itemAt(mapped.x, mapped.y);
+
+            if (item) {
+                listView.currentIndex = item.itemIndex;
+            } else {
+                listView.currentIndex = -1;
+            }
+
+            if (pressed && pressX != -1 && pressed.url && dragHelper.isDrag(pressX, pressY, mouse.x, mouse.y)) {
+                kickoff.dragSource = item;
+                dragHelper.startDrag(root, pressed.url, pressed.decoration);
+                pressed = null;
+                pressX = -1;
+                pressY = -1;
+            }
+        }
+
+        onContainsMouseChanged: {
+            if (!containsMouse) {
+                pressed = null;
+                pressX = -1;
+                pressY = -1;
+            }
+        }
+    }
+}

package/contents/ui/ApplicationsView.qml
@@ -2,7 +2,7 @@
     Copyright (C) 2011  Martin Gräßlin <mgraesslin@kde.org>
     Copyright (C) 2012  Gregor Taetzner <gregor@freenet.de>
     Copyright 2014 Sebastian Kügler <sebas@kde.org>
-    Copyright (C) 2015  Eike Hein <hein@kde.org>
+    Copyright (C) 2015-2018  Eike Hein <hein@kde.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -30,7 +30,7 @@ Item {
 
     objectName: "ApplicationsView"
 
-    property ListView listView: applicationsView
+    property ListView listView: applicationsView.listView
 
     function decrementCurrentIndex() {
         applicationsView.decrementCurrentIndex();
@@ -46,7 +46,7 @@ Item {
                 return;
             }
         }
-        appViewScrollArea.state = "OutgoingLeft";
+        applicationsView.state = "OutgoingLeft";
     }
 
     function openContextMenu() {
@@ -56,7 +56,7 @@ Item {
     function deactivateCurrentIndex() {
         if (crumbModel.count > 0) { // this is not the case when switching from the "Applications" to the "Favorites" tab using the "Left" key
             breadcrumbsElement.children[crumbModel.count-1].clickCrumb();
-            appViewScrollArea.state = "OutgoingRight";
+            applicationsView.state = "OutgoingRight";
             return true;
         }
         return false;
@@ -64,7 +64,7 @@ Item {
 
     function reset() {
         applicationsView.model = rootModel;
-        applicationsView.positionViewAtBeginning();
+        applicationsView.listView.positionViewAtBeginning();
         applicationsView.clearBreadcrumbs();
     }
 
@@ -145,10 +145,8 @@ Item {
         } // Flickable
     } // crumbContainer
 
-    PlasmaExtras.ScrollArea {
-        id: appViewScrollArea
-
-        property Item activatedItem: null
+    KickoffListView {
+        id: applicationsView
 
         anchors {
             top: crumbContainer.bottom
@@ -157,60 +155,50 @@ Item {
             leftMargin: -units.largeSpacing
         }
 
-        Behavior on opacity { NumberAnimation { duration: units.longDuration } }
-
         width: parent.width
 
-        function moveRight() {
-            state = "";
-            activatedItem.activate()
-            applicationsView.positionViewAtBeginning()
-        }
+        property Item activatedItem: null
+        property var newModel: null
+
+        Behavior on opacity { NumberAnimation { duration: units.longDuration } }
+
+        focus: true
+
+        appView: true
+
+        model: rootModel
 
         function moveLeft() {
             state = "";
             // newModelIndex set by clicked breadcrumb
             var oldModel = applicationsView.model;
             applicationsView.model = applicationsView.newModel;
-            applicationsView.positionViewAtIndex(applicationsView.model.rowForModel(oldModel), ListView.Center)
+            listView.positionViewAtIndex(model.rowForModel(oldModel), ListView.Center)
         }
 
-        ListView {
-            id: applicationsView
+        function moveRight() {
+            state = "";
+            activatedItem.activate()
+            applicationsView.listView.positionViewAtBeginning()
+        }
 
-            property variant newModel
+        function clearBreadcrumbs() {
+            crumbModel.clear();
+            crumbModel.models = [];
+        }
 
-            focus: true
-            keyNavigationWraps: true
-            boundsBehavior: Flickable.StopAtBounds
-            highlight: KickoffHighlight {}
-            highlightMoveDuration : 0
-            highlightResizeDuration: 0
+        onReset: appViewContainer.reset()
 
-            model: rootModel
-
-            delegate: KickoffItem {
-                id: kickoffItem
-
-                appView: true
-            }
-
-            function addBreadcrumb(model, title) {
-                crumbModel.append({"text": title, "depth": crumbModel.count+1})
-                crumbModel.models.push(model);
-            }
-
-            function clearBreadcrumbs() {
-                crumbModel.clear();
-                crumbModel.models = [];
-            }
-        } // applicationsView
+        onAddBreadcrumb: {
+            crumbModel.append({"text": title, "depth": crumbModel.count+1})
+            crumbModel.models.push(model);
+        }
 
         states: [
             State {
                 name: "OutgoingLeft"
                 PropertyChanges {
-                    target: appViewScrollArea
+                    target: applicationsView
                     x: -parent.width
                     opacity: 0.0
                 }
@@ -218,7 +206,7 @@ Item {
             State {
                 name: "OutgoingRight"
                 PropertyChanges {
-                    target: appViewScrollArea
+                    target: applicationsView
                     x: parent.width
                     opacity: 0.0
                 }
@@ -232,24 +220,26 @@ Item {
                     // We need to cache the currentItem since the selection can move during animation,
                     // and we want the item that has been clicked on, not the one that is under the
                     // mouse once the animation is done
-                    ScriptAction { script: appViewScrollArea.activatedItem = applicationsView.currentItem }
+                    ScriptAction { script: applicationsView.activatedItem = applicationsView.currentItem }
                     NumberAnimation { properties: "x,opacity"; easing.type: Easing.InQuad; duration: units.longDuration }
-                    ScriptAction { script: appViewScrollArea.moveRight() }
+                    ScriptAction { script: applicationsView.moveRight() }
                 }
             },
             Transition {
                 to: "OutgoingRight"
                 SequentialAnimation {
                     NumberAnimation { properties: "x,opacity"; easing.type: Easing.InQuad; duration: units.longDuration }
-                    ScriptAction { script: appViewScrollArea.moveLeft() }
+                    ScriptAction { script: applicationsView.moveLeft() }
                 }
             }
         ]
-    } // appViewScrollArea
+    }
 
     MouseArea {
-        anchors.fill: appViewScrollArea
+        anchors.fill: parent
+
         acceptedButtons: Qt.BackButton
+
         onClicked: {
             deactivateCurrentIndex()
         }
@@ -265,13 +255,13 @@ Item {
             if (running) {
                 updatedLabel.opacity = 1;
                 crumbContainer.opacity = 0.3;
-                appViewScrollArea.opacity = 0.3;
+                applicationsView.scrollArea.opacity = 0.3;
             }
         }
         onTriggered: {
             updatedLabel.opacity = 0;
             crumbContainer.opacity = 1;
-            appViewScrollArea.opacity = 1;
+            applicationsView.scrollArea.opacity = 1;
             running = false;
         }
     }

package/contents/ui/BaseView.qml
@@ -1,7 +1,7 @@
 /*
     Copyright (C) 2011  Martin Gräßlin <mgraesslin@kde.org>
     Copyright (C) 2012 Marco Martin <mart@kde.org>
-    Copyright (C) 2015  Eike Hein <hein@kde.org>
+    Copyright (C) 2015-2018  Eike Hein <hein@kde.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -25,56 +25,43 @@ import org.kde.draganddrop 2.0
 
 
 Item {
-    property alias model: kickoffListView.model
-    property alias delegate: kickoffListView.delegate
+    property alias model: baseView.model
+    property alias delegate: baseView.delegate
 
-    property ListView listView: kickoffListView
+    property ListView listView: baseView.listView
 
     function decrementCurrentIndex() {
-        kickoffListView.decrementCurrentIndex();
+        baseView.decrementCurrentIndex();
     }
 
     function incrementCurrentIndex() {
-        kickoffListView.incrementCurrentIndex();
+        baseView.incrementCurrentIndex();
     }
 
     function activateCurrentIndex() {
-        kickoffListView.currentItem.activate();
+        baseView.currentItem.activate();
     }
 
     function openContextMenu() {
-        kickoffListView.currentItem.openActionMenu();
+        baseView.currentItem.openActionMenu();
     }
 
-    PlasmaExtras.ScrollArea {
-        anchors.fill: parent
+    Connections {
+        target: plasmoid
 
-        ListView {
-            id: kickoffListView
-
-            interactive: contentHeight > height
-            boundsBehavior: Flickable.StopAtBounds
-            currentIndex: -1
-            keyNavigationWraps: true
-            highlight: KickoffHighlight {}
-            highlightMoveDuration : 0
-            highlightResizeDuration: 0
-
-            delegate: KickoffItem {}
-
-            section {
-                property: "group"
-                criteria: ViewSection.FullString
-                delegate: SectionDelegate {}
-            }
-            Connections {
-                target: plasmoid
-                onExpandedChanged: {
-                    if (!expanded) {
-                        kickoffListView.currentIndex = -1;
-                    }
-                }
+        onExpandedChanged: {
+            if (!expanded) {
+                baseView.currentIndex = -1;
             }
         }
+    }
+
+    KickoffListView {
+        id: baseView
+
+        anchors.fill: parent
+
+        currentIndex: -1
+        interactive: contentHeight > height
     }
 }

package/contents/ui/Breadcrumb.qml
@@ -53,7 +53,7 @@ Item {
             } else {
                 applicationsView.newModel = crumbModel.models[index];
             }
-            appViewScrollArea.state = "OutgoingRight";
+            applicationsView.state = "OutgoingRight";
         }
     }
 

package/contents/ui/FavoritesView.qml
@@ -2,7 +2,7 @@
     Copyright (C) 2011  Martin Gräßlin <mgraesslin@kde.org>
     Copyright (C) 2012 Marco Martin <mart@kde.org>
     Copyright 2014 Sebastian Kügler <sebas@kde.org>
-    Copyright (C) 2015  Eike Hein <hein@kde.org>
+    Copyright (C) 2015-2018  Eike Hein <hein@kde.org>
     Copyright (C) 2016 Jonathan Liu <net147@gmail.com>
     Copyright (C) 2016 Kai Uwe Broulik <kde@privat.broulik.de>
 
@@ -35,22 +35,22 @@ Item {
 
     objectName: "FavoritesView"
 
-    property ListView listView: kickoffListView
+    property ListView listView: favoritesView.listView
 
     function decrementCurrentIndex() {
-        kickoffListView.decrementCurrentIndex();
+        favoritesView.decrementCurrentIndex();
     }
 
     function incrementCurrentIndex() {
-        kickoffListView.incrementCurrentIndex();
+        favoritesView.incrementCurrentIndex();
     }
 
     function activateCurrentIndex() {
-        kickoffListView.currentItem.activate();
+        favoritesView.currentItem.activate();
     }
 
     function openContextMenu() {
-        kickoffListView.currentItem.openActionMenu();
+        favoritesView.currentItem.openActionMenu();
     }
 
     // QQuickItem::isAncestorOf is not invokable...
@@ -69,94 +69,77 @@ Item {
     DropArea {
         property int startRow: -1
 
-        anchors.fill: scrollArea
+        anchors.fill: parent
         enabled: plasmoid.immutability !== PlasmaCore.Types.SystemImmutable
 
         function syncTarget(event) {
-            if (kickoffListView.animating) {
+            if (favoritesView.animating) {
                 return;
             }
 
-            var pos = mapToItem(kickoffListView.contentItem, event.x, event.y);
-            var above = kickoffListView.itemAt(pos.x, pos.y);
+            var pos = mapToItem(listView.contentItem, event.x, event.y);
+            var above = listView.itemAt(pos.x, pos.y);
 
             var source = kickoff.dragSource;
 
-            if (above && above !== source && isChildOf(source, kickoffListView)) {
-                kickoffListView.model.moveRow(source.itemIndex, above.itemIndex);
+            if (above && above !== source && isChildOf(source, favoritesView)) {
+                favoritesView.model.moveRow(source.itemIndex, above.itemIndex);
                 // itemIndex changes directly after moving,
                 // we can just set the currentIndex to it then.
-                kickoffListView.currentIndex = source.itemIndex;
+                favoritesView.currentIndex = source.itemIndex;
             }
         }
 
         onDragEnter: {
             syncTarget(event);
-            startRow = kickoffListView.currentIndex;
+            startRow = favoritesView.currentIndex;
         }
 
-        onDragMove: syncTarget(event);
+        onDragMove: syncTarget(event)
     }
 
     Transition {
         id: moveTransition
         SequentialAnimation {
-            PropertyAction { target: kickoffListView; property: "animating"; value: true }
+            PropertyAction { target: favoritesView; property: "animating"; value: true }
 
             NumberAnimation {
-                duration: kickoffListView.animationDuration
+                duration: favoritesView.animationDuration
                 properties: "x, y"
                 easing.type: Easing.OutQuad
             }
 
-            PropertyAction { target: kickoffListView; property: "animating"; value: false }
+            PropertyAction { target: favoritesView; property: "animating"; value: false }
         }
     }
 
-    PlasmaExtras.ScrollArea {
-        id: scrollArea
+    Connections {
+        target: plasmoid
+        onExpandedChanged: {
+            if (!expanded) {
+                favoritesView.currentIndex = -1;
+            }
+        }
+    }
+
+    KickoffListView {
+        id: favoritesView
 
         anchors.fill: parent
 
-        ListView {
-            id: kickoffListView
+        property bool animating: false
+        property int animationDuration: resetAnimationDurationTimer.interval
 
-            property bool animating: false
-            property int animationDuration: resetAnimationDurationTimer.interval
+        interactive: contentHeight > height
 
-            currentIndex: -1
-            boundsBehavior: Flickable.StopAtBounds
-            keyNavigationWraps: true
-            interactive: contentHeight > height
+        move: moveTransition
+        moveDisplaced: moveTransition
 
-            delegate: KickoffItem {}
-            highlight: KickoffHighlight {}
-            highlightMoveDuration : 0
-            highlightResizeDuration: 0
+        model: globalFavorites
 
-            model: globalFavorites
-
-            onCountChanged: {
-                animationDuration = 0;
-                resetAnimationDurationTimer.start();
-            }
-
-            section {
-                property: "group"
-                criteria: ViewSection.FullString
-            }
-
-            move: moveTransition
-            moveDisplaced: moveTransition
-
-            Connections {
-                target: plasmoid
-                onExpandedChanged: {
-                    if (!expanded) {
-                        kickoffListView.currentIndex = -1;
-                    }
-                }
-            }
+        onCountChanged: {
+            animationDuration = 0;
+            resetAnimationDurationTimer.start();
         }
     }
 
@@ -165,7 +148,7 @@ Item {
 
         interval: 150
 
-        onTriggered: kickoffListView.animationDuration = interval - 20
+        onTriggered: favoritesView.animationDuration = interval - 20
     }
 
 }

commit 3566242162647b60e5fa13b1155de1de4c5e8d19

Fix mouse events being withheld from the scrollbar.
Summary:
Fixes regression introduced in ab83a41d2ad2.

Thanks to ngraham for pointing out the bug.

Reviewers: ngraham

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D17003

package/contents/ui/KickoffListView.qml
@@ -91,7 +91,10 @@ FocusScope {
     }
 
     MouseArea {
-        anchors.fill: parent
+        anchors.left: parent.left
+
+        width: scrollArea.viewport.width
+        height: parent.height
 
         id: mouseArea
 